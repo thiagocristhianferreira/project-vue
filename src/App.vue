eb8f7b9839a50b1b57dd487568b7c5e26b9774fb
Thiago Ferreira <thiagocristhian@gmail.com>
2021-11-18 17:28:10 -0300

    Criando componente de adicionar comentários

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="imagem" alt="Vue logo" src="./assets/logo.png">
+    <Comments />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Comments from './components/Comments.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Comments
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .imagem {
+    display: block;
+    margin: auto;
+  }
 </style>
---
+++ src/components/Comments.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <h1>Comentários</h1>
+    <hr />
+    <div class="form-todo form-group">
+      <p>
+        <input placeholder="nome" type="text" name="author" class="form-control" />
+      </p>
+      <p>
+        <textarea placeholder="Comentário" name="message"  class="form-control"></textarea>
+      </p>
+      <button type="submit" class="btn btn-primary">Comentar</button>
+    </div>
+    <div class="list-group">
+      <div class="list-group-item">
+        <span class="comment__author">Autor: <strong>Primeiro autor</strong></span>
+        <p>Primeiro Comentário</p>
+        <div>
+          <a href="#" title="Excluir">Excluir</a>
+        </div>
+      </div>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      comments: [],
+      name: '',
+      message: '',
+    }
+  }
+}
+</script>
+
